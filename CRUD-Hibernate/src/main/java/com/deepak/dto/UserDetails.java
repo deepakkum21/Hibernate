e299d49881f4ac30104e0852216e698cb22940d1
Deepak Kumar <deepak.kumar@sysbiz.com>
2019-12-09 18:54:08 +0530

    updated with @SelectBeforeUpdate(true)

--- CRUD-Hibernate/src/main/java/com/deepak/dto/UserDetails.java
+++ CRUD-Hibernate/src/main/java/com/deepak/dto/UserDetails.java
@@ -5,7 +5,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import org.hibernate.annotations.SelectBeforeUpdate;
+
 @Entity
+// Should the entity's current state be selected from the database when determining whether to perform an update when
+// re-attaching detached entities?
+@SelectBeforeUpdate(true)                          // fires select query before firing update query, if there is changes in the record then only fires update query
 public class UserDetails {
 
 	@Id
